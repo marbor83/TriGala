f314c8fabbc1ee2eebbf121cca6eada1b66c3770
Valerioc <valerio.casale@avangarde.it>
2015-09-11 11:23:14 +0200

    2

--- SCRIPT SQL/20150722/D12_CreateSP_SetEsposizione.sql
+++ SCRIPT SQL/20150722/D12_CreateSP_SetEsposizione.sql
@@ -1,32 +1,31 @@
-USE [dbDatamaxGALA]
-GO
-/****** Object:  StoredProcedure [GALA_CB].[CB_Sp_SetEsposizione]    Script Date: 09/05/2015 20:07:11 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-/* ISTRUZIONI SQL SENZA GO */
-ALTER PROCEDURE [GALA_CB].[CB_Sp_SetEsposizione]
+CREATE PROCEDURE [GALA_CB].[CB_Sp_SetEsposizione]
 	@DataDa Datetime,
 	@DataA  Datetime,
 	@IdCliente INT
 AS
 BEGIN
 	
+	create table #TempAnag
+	(
+		idAnagrafica INT,
+		SaldoScaduto Numeric(18,2),
+		SaldoAScadere Numeric(18,2),
+		SaldoExtra Numeric(18,2),
+		STATO_LAVORAZIONE INT
+
+	)
+	
+	
 	create table #TempEsposizioni
 	(
 		idAnagrafica INT,
 		SaldoScaduto Numeric(18,2),
-		SaldoAScadenza Numeric(18,2),
-		SaldoExtra Numeric(18,2),
-		STATO_LAVORAZIONE INT
 	)
 	
 	create table #TempEsposizioni2
 	(
 		idAnagrafica INT,
-		SaldoAScadenza Numeric(18,2)
+		SaldoAScadere Numeric(18,2)
 	)
 	
 		create table #TempEsposizioni3
@@ -34,52 +33,72 @@ BEGIN
 		idAnagrafica INT,
 		SaldoExtra Numeric(18,2)
 	)
+	
+	
+	-- Tutti gli idAnagrafica--
+	insert into #TempAnag select a.idAnagrafica, 0 , 0, 0, 
+	(Case WHEN (Select COUNT(*) 
+				from Reclami r 
+				where r.IDAnagrafica = a.IdAnagrafica 
+					and r.IDStatoReclamo in(1,3) 
+					and r.IDMotivoReclamo in (Select IDMotivoReclamo 
+												from Ticket_SospensioneCredito)
+	 ) > 0 THEN 1 ELSE 0 END) as STATO_LAVORAZIONE  
+	from Anagrafica a
+	where a.IDStatoAnagrafica=1 -- Cliente Attivo (con 2 il cliente non è visualizzabile)
+		and a.IDAnagrafica!='100001'
+	
 
 	
-	insert into #TempEsposizioni (idAnagrafica,SaldoScaduto, STATO_LAVORAZIONE)
+	-- Calcolo Saldo Scaduto --
+	insert into #TempEsposizioni (idAnagrafica,SaldoScaduto)
 	
 	select    s.IDAnagrafica,
-                sum(m.Importo) SaldoScaduto,
-                (Case WHEN (Select COUNT(*) from Reclami r where r.IDAnagrafica = s.IdAnagrafica and r.IDMotivoReclamo in (Select IDMotivoReclamo from Ticket_SospensioneCredito)) > 0 THEN 1 ELSE 0 END) as STATO_LAVORAZIONE 
-                
+                sum(m.Importo) SaldoScaduto
 		from      Tes.Movimenti m
 			inner join Scadenzario s on s.IDFattura=m.IDFattura
 		where  m.IDAnagrafica=s.IDAnagrafica 
                 and m.IDStato>=0 
-                                and s.DataScad < getdate()
-        
-		group by s.IDAnagrafica order by s.IDAnagrafica
+                and s.DataScad < getdate()        
+		group by s.IDAnagrafica
 	
+		update T  set T.SaldoScaduto = OT.SaldoScaduto
+		from #TempAnag T inner join #TempEsposizioni OT on T.idAnagrafica = OT.idAnagrafica
 	
-	insert into #TempEsposizioni2 (idAnagrafica, SaldoAScadenza)
+	-- Calcolo Saldo A Scadere --
+	insert into #TempEsposizioni2 (idAnagrafica, SaldoAScadere)
 			select    s.IDAnagrafica,
-							sum(m.Importo) SaldoAScadenza
+				      sum(m.Importo) SaldoAScadere
 			from      Tes.Movimenti m
 			inner join Scadenzario s on s.IDFattura=m.IDFattura  
 			where  m.IDAnagrafica=s.IDAnagrafica 
-							and m.IDStato>=0 
-											and s.DataScad >= getdate()
+				   and m.IDStato>=0 
+				   and s.DataScad >= getdate()
 			group by s.IDAnagrafica
 	
-	update T  set T.SaldoAScadenza = OT.SaldoAScadenza
-		from #TempEsposizioni T inner join #TempEsposizioni2 OT on T.idAnagrafica = OT.idAnagrafica
+	update T  set T.SaldoAScadere = OT.SaldoAScadere
+		from #TempAnag T inner join #TempEsposizioni2 OT on T.idAnagrafica = OT.idAnagrafica
 	
-	 
+	-- Calcolo Saldo Extra --
 	insert into #TempEsposizioni3 (idAnagrafica, SaldoExtra)
 			select    IDAnagrafica,
-							sum(Importo) SaldoExtra
+					  sum(Importo) SaldoExtra
 			from      Tes.Movimenti
 			where  IDStato>=0 
-							and IDFattura is null
+				   and IDFattura is null
 			group by IDAnagrafica
 	
 	update T  set T.SaldoExtra = OT.SaldoExtra
-		from #TempEsposizioni T inner join #TempEsposizioni3 OT on T.idAnagrafica = OT.idAnagrafica
+		from #TempAnag T inner join #TempEsposizioni3 OT on T.idAnagrafica = OT.idAnagrafica
 	
 	
-	select 'IT10' as ID_AZIENDA, e.idAnagrafica as ID_CLIENTE,( e.SaldoScaduto + ISNULL(e.SaldoAScadenza, 0) + ISNULL(e.SaldoExtra, 0)) as Saldo , ISNULL(e.SaldoExtra, 0) as Extra_Saldo, e.STATO_LAVORAZIONE From #TempEsposizioni e 
-		where e.idAnagrafica = ISNULL(@IdCliente,e.idAnagrafica) --   122952
-		
+	-- Return dei valori estratti--
+	select 'IT10' as ID_AZIENDA, e.idAnagrafica as ID_CLIENTE,( e.SaldoScaduto + ISNULL(e.SaldoAScadere, 0)) as Saldo , ISNULL(e.SaldoExtra, 0) as Extra_Saldo, e.STATO_LAVORAZIONE 
+	From #TempAnag e 
+		where e.idAnagrafica = ISNULL(@IdCliente,e.idAnagrafica) 
+	
+	--Drop Temp Table--
+	drop table #TempAnag
 	drop table #TempEsposizioni
 	drop table #TempEsposizioni2
 	drop table #TempEsposizioni3
