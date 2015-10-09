301a82154df6b2d3158668ec6cbe7e390993171e
marbor83 <borzellim@gmail.com>
2015-10-09 13:01:24 +0200

    1

--- SCRIPT SQL/20150722/D100_Datamax_Script_Transazionale_Base.sql
+++ SCRIPT SQL/20150722/D100_Datamax_Script_Transazionale_Base.sql
@@ -867,7 +867,8 @@ BEGIN
 			a.RagSoc as RAGIONESOCIALE,
 			a.Indirizzo as INDIRIZZO,
 			a.CAP as CAP,
-			a.Localita as COMUNE,
+			CASE WHEN a.Localita is Null THEN GALA_CB.GetIndirizzoCliente (a.IDAnagrafica) else a.Localita END as COMUNE,			
+			--a.Localita as COMUNE,
 			a.Provincia as PROVINCIA,
 			a.Nazione as NAZIONE,
 			a.NumeroFax as Fax,
@@ -1534,6 +1535,8 @@ CREATE PROCEDURE [GALA_CB].[CB_Sp_GetAnagraficaMovimenti]
 AS
 BEGIN
 
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
 	DECLARE @DataDaExtraSaldo DATETIME
 	 Select @DataDaExtraSaldo =  CAST( 
 						cast(year(@DataA) as  varchar)+'/'+ 
@@ -1541,7 +1544,41 @@ BEGIN
 						cast(day(@DataA) as  varchar) as datetime)
 
 
-Select * into #tmpMovimenti from(
+
+	SELECT	A.IDTBilling,
+			A.IDFattura,
+			A.NumDocsuFatt AS 'Numero_Documento',
+			CONVERT(DATE,A.DataDoc) AS Emissione,
+			CONVERT(DATE,A.DataScad) AS Scadenza,
+			CONVERT(DATE,B.ScadenzaRicalcolata) AS 'Scadenza_Ricalcolata'
+	INTO		#FattureFEPA
+	FROM		dbDatamaxGALA.dbo.Scadenzario A
+	INNER JOIN	BillingTGALA.dbo.vw_FattPA_DataRicevutaConsegnaScadenza B		ON A.IDTBilling = B.idDocT
+	LEFT JOIN (	SELECT		IDDocT, 'SI' AS 'Split'
+				FROM		BillingTGALA.dbo.DocIVA
+				WHERE		IDCodiceIVA Like ('%D%')
+				GROUP BY	IDDocT) C											ON A.IDTBilling = C.IDDocT
+
+
+	SELECT  distinct
+		A.IDTBilling, A.IDFattura, A.[Numero_Documento], A.Emissione, Scadenza, [Scadenza_Ricalcolata], ES.DataOraLetturaEsito
+		into #FattureFEPA_OK
+		FROM		#FattureFEPA as A
+		OUTER APPLY BillingTGALA.dbo.fnPA_GetStatoFattura_ext(A.IDTBilling) ES
+		WHERE		ES.MacroEsito = 'OK'
+		AND A.[Scadenza_Ricalcolata] Is Not Null
+		
+		
+	SELECT  distinct
+		A.IDTBilling, A.IDFattura, A.[Numero_Documento], A.Emissione, Scadenza, [Scadenza_Ricalcolata], DataOraLetturaEsito
+		into #FattureFEPA_OK_UPD
+		FROM		#FattureFEPA_OK as A
+		WHERE		DataOraLetturaEsito between @DataDa and @DataA
+		
+
+
+
+Select distinct * into #tmpMovimenti from(
 select 	m.idMovimento as Id_Record,
 		'IT10' as id_Azienda,		
 		s.IDAnagrafica as Id_Cliente,
@@ -1587,7 +1624,7 @@ select 	m.idMovimento as Id_Record,
 		doct.IDDocT as IDDocT,
 		m.IDStato as ID_STATO,
 		m.IDFattura as ID_FATTURA,
-		null as DATA_SCAD_RICALCOLATA
+		CASE WHEN c.idCausale in (1,7) THEN fepa.Scadenza_Ricalcolata ELSE null END as DATA_SCAD_RICALCOLATA
 from Tes.Movimenti m 
 inner join Scadenzario s  on m.IDAnagrafica = s.IDAnagrafica and m.IDFattura=s.IDFattura
 inner join dbo.Anagrafica a on m.IDAnagrafica=a.IDAnagrafica 
@@ -1596,19 +1633,84 @@ LEFT JOIN	BillingTGALA.dbo.DocT bill	ON s.IDTBilling = bill.IDDocT
 LEFT JOIN	dbo.DocT doct	ON s.IDTBilling = doct.IDDocT
 left join dbo.TipiPagamento tp on tp.IDTipoPagamento = doct.idMetodoPag
 left join GALA_CB.GALA_DESCRIZIONE_TIPO_FATTURA gdf on s.TipoDoc = gdf.IdTipoDOC
+left join #FattureFEPA_OK fepa on fepa.IDFattura = m.IDFattura 
 where	a.IDStatoAnagrafica=1
 		--and s.IDFattura is null
 		and m.IDAnagrafica!='100001'
-		and m.IDStato>=0
-		and isnull(bill.IDDocFlusso,0) not in (select A.IDDocFlusso FROM	dbDatamaxGALA.dbo.FlussoDaEscludereScadenzario A)
+		and (m.IDStato>=0 or (m.IDStato<0 and m.DATUMO between @DataDa and @DataA))
+		--and isnull(bill.IDDocFlusso,0) not in (select A.IDDocFlusso FROM	dbDatamaxGALA.dbo.FlussoDaEscludereScadenzario A)
 		and exists (select	1 
 					from	dbo.Contratti contr
 					inner join dbo.ContrattiRighe cr on contr.IDContratto_Cnt=cr.IDContratto_Cnt
 					where	contr.IDAnagrafica=m.IDAnagrafica
 							and cr.IDStatoRiga != 11)
-							--and getdate() between cr.DataInizioValidita and coalesce(cr.DataCessazione, cr.dataFineValidita, '20501231'))
-		AND m.DatPMO between @DataDa and @DataA
-		and m.IDAnagrafica = ISNULL(@IdCliente, m.IDAnagrafica)		
+		AND (m.DatPMO between @DataDa and @DataA OR m.DATUMO between @DataDa and @DataA)
+		and m.IDAnagrafica = ISNULL(@IdCliente, m.IDAnagrafica)
+union
+select 	m.idMovimento as Id_Record,
+		'IT10' as id_Azienda,		
+		s.IDAnagrafica as Id_Cliente,
+		s.NumDocSuFatt as N_DOC,
+		s.DataDoc as DATA_DOC,
+		m.DATPMO as DATA_REG,
+		m.DATPMO as Data_Acq,
+		m.DATUMO as Data_Upd,
+		s.DataScad DATA_SCAD,
+		CASE WHEN ISNULL(m.importo, 0) < 0 THEN 'A' ELSE 'D' END as Segno,	
+		'EUR' as Valuta,
+		m.importo as IMPORTO,
+		Convert(numeric(16,6), bill.ValImponibile) as IMPONIBILE,
+		m.IDCausale as ID_CAUSALE,
+		c.descrizione DESCR_CAUSALE,		
+		CASE WHEN c.idCausale in (1,7) THEN doct.idMetodoPag  ELSE null END as ID_PAG_MOD,
+		CASE WHEN c.idCausale in (1,7) THEN tp.descrizione  ELSE null END as DESCR_PAG_MOD,		
+		null as ID_PAG_TER,
+		null as DESCR_PAG_TER,
+		(CASE	WHEN s.SiglaRegIVA In ('E','T') THEN 'EE'
+				WHEN s.SiglaRegIVA In ('G','S') THEN 'GAS'
+				WHEN s.SiglaRegIVA In ('C','I','N','O') THEN 'SERV'
+				ELSE 'EE' END) as COMMODITY, 
+		m.idFattura as CODICE_PARTITA,
+		null as CODICE_PARTITA_STORNO,
+		null as Factor,
+		s.TipoDoc as Tipologia_fattura,		
+		gdf.descrizione as  Desscrizione_tipologia_fattura,
+		('\\VMTRIMP\Fatture\Attive\' +
+			(CASE	WHEN s.SiglaRegIVA In ('E','T') THEN 'EE'
+					WHEN s.SiglaRegIVA In ('G','S') THEN 'GAS'
+					WHEN s.SiglaRegIVA In ('C','I','N','O') THEN 'SERV'
+					ELSE 'EE' END) +
+			'\' + s.IDAnagrafica + '\' + CONVERT(VARCHAR(4),bill.AnnoDoc) + '_' +
+					(CASE	WHEN LEN(s.NumeroDoc) = 1 THEN '0000000'
+							WHEN LEN(s.NumeroDoc) = 2 THEN '000000'
+							WHEN LEN(s.NumeroDoc) = 3 THEN '00000'
+							WHEN LEN(s.NumeroDoc) = 4 THEN '0000'
+							WHEN LEN(s.NumeroDoc) = 5 THEN '000'
+							WHEN LEN(s.NumeroDoc) = 6 THEN '00'
+							WHEN LEN(s.NumeroDoc) = 7 THEN '0'
+							ELSE '' END) +	s.NumeroDoc + s.SiglaRegIVA + '.pdf') as URL,
+		doct.IDDocT as IDDocT,
+		m.IDStato as ID_STATO,
+		m.IDFattura as ID_FATTURA,
+		CASE WHEN c.idCausale in (1,7) THEN fepa.Scadenza_Ricalcolata ELSE null END as DATA_SCAD_RICALCOLATA
+from Tes.Movimenti m 
+inner join Scadenzario s  on m.IDAnagrafica = s.IDAnagrafica and m.IDFattura=s.IDFattura
+inner join dbo.Anagrafica a on m.IDAnagrafica=a.IDAnagrafica 
+inner join tes.causali c on m.idCausale = c.idCausale
+LEFT JOIN	BillingTGALA.dbo.DocT bill	ON s.IDTBilling = bill.IDDocT
+LEFT JOIN	dbo.DocT doct	ON s.IDTBilling = doct.IDDocT
+left join dbo.TipiPagamento tp on tp.IDTipoPagamento = doct.idMetodoPag
+left join GALA_CB.GALA_DESCRIZIONE_TIPO_FATTURA gdf on s.TipoDoc = gdf.IdTipoDOC
+inner join #FattureFEPA_OK_UPD fepa on fepa.IDFattura = m.IDFattura 
+where	a.IDStatoAnagrafica=1
+		and m.IDAnagrafica!='100001'
+		and m.IDStato>=0
+		and exists (select	1 
+					from	dbo.Contratti contr
+					inner join dbo.ContrattiRighe cr on contr.IDContratto_Cnt=cr.IDContratto_Cnt
+					where	contr.IDAnagrafica=m.IDAnagrafica
+							and cr.IDStatoRiga != 11)
+		and m.IDAnagrafica = ISNULL(@IdCliente, m.IDAnagrafica)
 union
 select 	m.IDAnagrafica as Id_Record,
 		'IT10' as id_Azienda,		
@@ -1645,10 +1747,10 @@ inner join dbo.Anagrafica a on m.IDAnagrafica=a.IDAnagrafica
 where	a.IDStatoAnagrafica=1
 		and m.IDFattura is null
 		and m.IDAnagrafica!='100001'
-		--and m.IDStato>=0
-		and m.DatPMO between @DataDaExtraSaldo and @DataA
-		--and exists (select 1 idAnagrafica from GALA_CB.GALA_ANAGRAFICA_CLIENTI_EXP_ATTIVI ct where ct.ID_CLIENTE = m.IdAnagrafica)
-		and m.IDAnagrafica = ISNULL(@IdCliente, m.IDAnagrafica)
+		and m.IDStato>=0
+		--and m.DatPMO between @DataDaExtraSaldo and @DataA
+		and m.DatPMO between @DataDa and @DataA
+		and m.IDAnagrafica = ISNULL(@IdCliente, m.IDAnagrafica)		
 		) as temp
 
 
@@ -1660,6 +1762,7 @@ FROM #tmpMovimenti
 
 
 UPDATE #tmpMovimenti2
+
 SET CODICE_PARTITA_STORNO = (select distinct d.idfattura  --id della fattura stornata
 							from docr c
 								 inner join scadenzario d on c.iddoctparent = d.idtbilling							
@@ -1691,11 +1794,21 @@ FROM #tmpMovimenti2
 
 DROP TABLE #tmpMovimenti
 DROP TABLE #tmpMovimenti2
+DROP TABLE #FattureFEPA_OK
+DROP TABLE #FattureFEPA_OK_UPD
+DROP TABLE #FattureFEPA
+
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
 
 END
-END
 
-
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 AND XACT_STATE() = -1 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0
+BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
 
 
 --##########################################################################
@@ -1749,6 +1862,47 @@ BEGIN
 	)
 	
 	
+	SELECT	A.IDTBilling,
+			A.IDFattura,
+			A.NumDocsuFatt AS 'Numero_Documento',
+			CONVERT(DATE,A.DataDoc) AS Emissione,
+			CONVERT(DATE,A.DataScad) AS Scadenza,
+			CONVERT(DATE,B.ScadenzaRicalcolata) AS 'Scadenza_Ricalcolata'
+	INTO		#FattureFEPA
+	FROM		dbDatamaxGALA.dbo.Scadenzario A
+	INNER JOIN	BillingTGALA.dbo.vw_FattPA_DataRicevutaConsegnaScadenza B	ON A.IDTBilling = B.idDocT
+	LEFT JOIN (	SELECT		IDDocT, 'SI' AS 'Split'
+				FROM		BillingTGALA.dbo.DocIVA
+				WHERE		IDCodiceIVA Like ('%D%')
+				GROUP BY	IDDocT) C	ON A.IDTBilling = C.IDDocT
+
+
+	SELECT  
+		A.IDTBilling, A.IDFattura, A.[Numero_Documento], A.Emissione, Scadenza, [Scadenza_Ricalcolata]
+		into #FattureFEPA_OK
+		FROM		#FattureFEPA as A
+		OUTER APPLY BillingTGALA.dbo.fnPA_GetStatoFattura_ext(A.IDTBilling) ES
+		WHERE		ES.MacroEsito = 'OK'
+		AND A.[Scadenza_Ricalcolata] Is Not Null
+
+
+	--SELECT IDTBilling, IDFattura, [Numero_Documento], Emissione, Scadenza, [Scadenza_Ricalcolata] 
+	--into #FattureFEPA_KO from (
+	--	SELECT	AA.IDTBilling, AA.IDFattura, AA.[Numero_Documento], AA.Emissione, Scadenza, [Scadenza_Ricalcolata]			
+	--	FROM		#FattureFEPA AA
+	--	OUTER APPLY BillingTGALA.dbo.fnPA_GetStatoFattura_ext(AA.IDTBilling) ES
+	--	WHERE		ES.MacroEsito = 'KO'
+	--	union
+	--	SELECT A.IDTBilling, A.IDFattura, A.[Numero_Documento], A.Emissione, Scadenza, [Scadenza_Ricalcolata]
+	--	FROM		#FattureFEPA A
+	--	OUTER APPLY BillingTGALA.dbo.fnPA_GetStatoFattura_ext(A.IDTBilling) ES
+	--	WHERE		ES.MacroEsito = 'OK'
+	--	AND A.[Scadenza_Ricalcolata] Is Null) fepaKO
+			
+		SELECT DISTINCT * INTO #FattureFEPA_OK_Dis  FROM #FattureFEPA_OK
+	
+	
+
 	-- Tutti gli idAnagrafica--
 	insert into #TempAnag select a.idAnagrafica, 0 , 0, 0, 
 	(Case WHEN (Select COUNT(*) 
@@ -1773,16 +1927,18 @@ BEGIN
 		from      Tes.Movimenti m
 			inner join Scadenzario s on s.IDFattura= m.IDFattura
 			LEFT JOIN BillingTGALA.dbo.DocT bill ON s.IDTBilling = bill.IDDocT
+			LEFT JOIN #FattureFEPA_OK_Dis fok ON m.idFattura = fok.idFattura
 		where  m.IDAnagrafica=s.IDAnagrafica 
                 and m.IDStato>=0
-				and isnull(bill.IDDocFlusso,0) not in (select A.IDDocFlusso FROM dbDatamaxGALA.dbo.FlussoDaEscludereScadenzario A)
+				--and isnull(bill.IDDocFlusso,0) not in (select A.IDDocFlusso FROM dbDatamaxGALA.dbo.FlussoDaEscludereScadenzario A)
 				and exists (select	1 
 							from	dbo.Contratti contr
 							inner join dbo.ContrattiRighe cr on contr.IDContratto_Cnt=cr.IDContratto_Cnt
 							where	contr.IDAnagrafica= m.IDAnagrafica
 								and cr.IDStatoRiga != 11)
-                and s.DataScad <= @DataA
+                and COALESCE(fok.Scadenza_Ricalcolata,s.DataScad) <= @DataA
                 and m.DatPMO <= @DataA
+                --and not exists (SELECT * FROM #FattureFEPA_KO fko WHERE fko.idFattura = m.IDFattura)
                 and m.idAnagrafica = ISNULL(@IdCliente,m.idAnagrafica)
 		group by s.IDAnagrafica
 	
@@ -1795,17 +1951,19 @@ BEGIN
 				      sum(m.Importo) SaldoAScadere
 			from      Tes.Movimenti m
 			inner join Scadenzario s on s.IDFattura= m.IDFattura
-			LEFT JOIN BillingTGALA.dbo.DocT bill ON s.IDTBilling = bill.IDDocT  
+			LEFT JOIN BillingTGALA.dbo.DocT bill ON s.IDTBilling = bill.IDDocT 
+			LEFT JOIN #FattureFEPA_OK_Dis fok ON m.idFattura = fok.idFattura 
 			where  m.IDAnagrafica=s.IDAnagrafica 
 				   and m.IDStato>=0
-				   and isnull(bill.IDDocFlusso,0) not in (select A.IDDocFlusso FROM dbDatamaxGALA.dbo.FlussoDaEscludereScadenzario A)
+				   --and isnull(bill.IDDocFlusso,0) not in (select A.IDDocFlusso FROM dbDatamaxGALA.dbo.FlussoDaEscludereScadenzario A)
 				   and exists (select	1 
 							from	dbo.Contratti contr
 							inner join dbo.ContrattiRighe cr on contr.IDContratto_Cnt=cr.IDContratto_Cnt
 							where	contr.IDAnagrafica= m.IDAnagrafica
 								and cr.IDStatoRiga != 11)
-				   and s.DataScad > @DataA
+				   and COALESCE(fok.Scadenza_Ricalcolata,s.DataScad) > @DataA
 				   and m.DatPMO <= @DataA
+				   --and not exists (SELECT * FROM #FattureFEPA_KO fko WHERE fko.idFattura = m.IDFattura)
 				   and m.idAnagrafica = ISNULL(@IdCliente,m.idAnagrafica)
 			group by s.IDAnagrafica
 	
@@ -1838,8 +1996,99 @@ BEGIN
 	
 END
 
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 AND XACT_STATE() = -1 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0
+BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
 --##########################################################################
 
+
+
+PRINT '17) Creazione Store Procedure CB_Sp_getFattureEscluse'
+GO
+
+IF EXISTS (SELECT * FROM sysobjects WHERE id=OBJECT_ID('GALA_CB.CB_Sp_getFattureEscluse')) 
+	DROP PROCEDURE GALA_CB.CB_Sp_getFattureEscluse
+GO
+
+CREATE PROCEDURE [GALA_CB].[CB_Sp_getFattureEscluse]
+	@DataDa Datetime,
+	@DataA  Datetime,
+	@IdCliente INT
+AS
+BEGIN
+	
+	
+	SELECT	A.IDTBilling,
+			A.IDFattura,
+			A.NumDocsuFatt AS 'Numero_Documento',
+			CONVERT(DATE,A.DataDoc) AS Emissione,
+			CONVERT(DATE,A.DataScad) AS Scadenza,
+			CONVERT(DATE,B.ScadenzaRicalcolata) AS 'Scadenza_Ricalcolata'
+	INTO		#FattureFEPA
+	FROM		dbDatamaxGALA.dbo.Scadenzario A
+	INNER JOIN	BillingTGALA.dbo.vw_FattPA_DataRicevutaConsegnaScadenza B	ON A.IDTBilling = B.idDocT
+	LEFT JOIN (	SELECT		IDDocT, 'SI' AS 'Split'
+				FROM		BillingTGALA.dbo.DocIVA
+				WHERE		IDCodiceIVA Like ('%D%')
+				GROUP BY	IDDocT) C	ON A.IDTBilling = C.IDDocT
+
+
+	SELECT IDTBilling, IDFattura, [Numero_Documento], Emissione, Scadenza, [Scadenza_Ricalcolata] 
+	into #FattureFEPA_KO from (
+		SELECT	AA.IDTBilling, AA.IDFattura, AA.[Numero_Documento], AA.Emissione, Scadenza, [Scadenza_Ricalcolata]			
+		FROM		#FattureFEPA AA
+		OUTER APPLY BillingTGALA.dbo.fnPA_GetStatoFattura_ext(AA.IDTBilling) ES
+		WHERE		ES.MacroEsito = 'KO'
+		union
+		SELECT A.IDTBilling, A.IDFattura, A.[Numero_Documento], A.Emissione, Scadenza, [Scadenza_Ricalcolata]
+		FROM		#FattureFEPA A
+		OUTER APPLY BillingTGALA.dbo.fnPA_GetStatoFattura_ext(A.IDTBilling) ES
+		WHERE		ES.MacroEsito = 'OK'
+		AND A.[Scadenza_Ricalcolata] Is Null) fepaKO
+			
+
+	
+	select distinct fko.IdFattura as ID_FATTURA, 
+	(SELECT IdAnagrafica from scadenzario 
+	where IDFattura = fko.idFattura) as ID_CLIENTE,
+	'FEPA_KO' as MOTIVAZIONE
+	INTO #FattureEscluse
+	from #FattureFEPA_KO fko
+	
+
+	-- SE UN FLUSSO DA ESCLUDERE E' ANCHE FEPA KO, LO VISUALIZZO COME FEPA KO
+	insert into #FattureEscluse (ID_FATTURA, ID_CLIENTE, MOTIVAZIONE)
+			select  distinct  s.idFattura,
+							  s.idAnagrafica,
+							  'ESCLUSA'
+			from      Scadenzario s 
+			inner JOIN BillingTGALA.dbo.DocT bill ON s.IDTBilling = bill.IDDocT 
+			where  isnull(bill.IDDocFlusso,0) in (select A.IDDocFlusso FROM dbDatamaxGALA.dbo.FlussoDaEscludereScadenzario A)
+			and Not exists (SELECT * FROM #FattureEscluse fe where fe.ID_FATTURA = s.idFattura)
+
+
+	SELECT * FROM #FattureEscluse
+	
+	--Drop Temp Table--
+	drop table #FattureFEPA
+	drop table #FattureFEPA_KO
+	drop table #FattureEscluse
+	
+END
+
+
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 AND XACT_STATE() = -1 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0
+BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+--##########################################################################
 
 
 
